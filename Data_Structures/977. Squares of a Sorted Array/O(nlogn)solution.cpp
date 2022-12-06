a6dbdbc61a3717be4a00fed0f0b5cb3bc56c8873
ikramuzzamansifat <95271488+ikramuzzamansifat@users.noreply.github.com>
2022-12-06 02:39:05 -0800

    Create O(nlogn)solution.cpp

---
+++ Data_Structures/977. Squares of a Sorted Array/O(nlogn)solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int l = nums.size();
+        for(int i=0; i<l; i++)
+        {
+            nums[i] *= nums[i];
+        }
+        sort(nums.begin(), nums.end());
+
+        return nums;
+    }
+};
